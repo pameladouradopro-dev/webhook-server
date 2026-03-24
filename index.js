7bb8edb39cfc4b137a93f8faa6772f76d1cf19ec
pameladouradopro-dev <pameladouradopro@gmail.com>
2026-03-24 10:33:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,8 @@ app.post("/webhook", async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Webhook ativo na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Webhook ativo na porta ${PORT}`);
 });
